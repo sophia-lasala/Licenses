6c949f0001bca7a9ab3cb7ae83e1f091accf28a9
Sophia LaSala <sophia.lasala@outlook.com>
2025-11-12 14:36:11 -0500

    Create Expiration.java

---
+++ src/Expiration.java
@@ -0,0 +1,63 @@
+package Streaming;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.time.LocalDate;
+
+
+public class Expiration {
+    private LinkedList<Movie> expirationList = new LinkedList<>();
+
+    public void expirationList(LinkedList<Movie> expirationList){
+        Data data = new Data();
+        ArrayList<Movie> movieList = new ArrayList<>();
+        data.movieList(movieList);
+
+        String title, licenseID;
+        LocalDate expirationDate, renewalDate;
+        long days;
+
+
+
+        for (int i = 0 ; i < movieList.size(); i++){
+            Movie temp = movieList.get(i);
+            title = temp.getTitle();
+            licenseID = temp.getLicenseID();
+            expirationDate = temp.getExpirationDate();
+            renewalDate = temp.getRenewalDate();
+
+            days = ChronoUnit.DAYS.between(renewalDate, expirationDate);
+
+            temp = new Movie(title, licenseID, expirationDate, renewalDate, days);
+            expirationList.add(temp);
+
+        }
+        orgExpirationList(expirationList);
+        printExpirationList(expirationList);
+    }
+
+    public void orgExpirationList(LinkedList<Movie> expirationList) {
+        int n = expirationList.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                Movie current = expirationList.get(j);
+                Movie next = expirationList.get(j + 1);
+
+                long x = current.getDays(), y = next.getDays();
+                if (x > y) {
+                    expirationList.set(j, next);
+                    expirationList.set(j + 1, current);
+                }
+            }
+        }
+    }
+
+    public static void printExpirationList(LinkedList<Movie> expirationList) {
+        System.out.println("Title | License ID | Expiration Date | Renewal Date | Days");
+
+        for (int i = 0; i < expirationList.size(); i++) {
+            Movie movie = expirationList.get(i);
+            System.out.println(movie.toExpirationString());
+        }
+    }
+}
