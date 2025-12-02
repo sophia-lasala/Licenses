86a10c13874191614fb3e6f56647a2bc9432704c
MovelikeLo <loicrobert640@gmail.com>
2025-12-01 19:39:59 -0500

    Update Main.java
    Added functionality to search for a movie by title and license ID.

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,24 @@ public class Main {
         LinkedList<Movie> expirationList = new LinkedList<>();
         Expiration.expirationList(expirationList);
         HistoryManager.printHistory();
+        Licenses lic = new Licenses();
+lic.buildAllStructures();
+
+Movie m = lic.findByLicenseID("LIC-93810");
+if (m != null) {
+    System.out.println("Found: " + m);
+}
+
+Licenses.printExpirationList(lic.getExpirationList());
+lic.printPendingQueue();
+        Tree titleTree = new Tree();
+titleTree.buildFromList(movieList);
+String searchTitle = "Inception";
+if (titleTree.containsTitle(searchTitle)) {
+    Movie found = titleTree.search(searchTitle);
+    System.out.println("Found movie: " + found);
+} else {
+    System.out.println(searchTitle + " is not in the system.");
+}
     }
 }
