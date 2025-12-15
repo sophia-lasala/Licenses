a6c04892f65bb0791b4d6e7306682ad4bd1c06e5
Sophia LaSala <sophia.lasala@outlook.com>
2025-12-15 12:38:05 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,45 @@
 package Streaming;
 
-
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         String userInput;
+        Data data = new Data ();
+        TitleTree titleTree = new TitleTree();
 
         System.out.println("Importing CSV file...");
-        Licenses licenses = new Licenses();
-        licenses.buildAllStructures();
-        Tree movieTree = new Tree();
-        movieTree.buildFromList(new ArrayList <>(licenses.getLicenseMap().values()));
+        data.buildAllStructures();
+        ArrayList<Movie> movieList = data.getMovieList();
+        LinkedList<Movie> expirationList = data.getExpirationList();
+        Queue<Movie> pendingQueue = data.getPendingQueue();
+
         System.out.println("Complete!");
 
         System.out.println("\nWould you like to: (1) Access all data (2) Access all licenses (3) Review licenses that need " +
-                "to be renewed soon (4) Access License IDs (5) Search for movies by Title (6) Access History (7) Exit");
+                "to be renewed soon (4) Access License IDs (5) Format Data (6) Access History (7) Exit");
 
         userInput = keyboard.next();
 
         while (!(userInput.equalsIgnoreCase("7"))) {
             switch (userInput) {
                 case "1":
-                    Data.printMovieList(new ArrayList<>(licenses.getLicenseMap().values()));
+                    data.printMovieList(movieList);
                     break;
                 case "2":
-                    Licenses.printExpirationList(licenses.getExpirationList());
+                    data.printExpirationList(expirationList);
                     break;
                 case "3":
-                    licenses.printPendingQueue();
+                    data.printPendingQueue();
                     break;
                 case "4":
                     System.out.println("Enter License ID:");
                     String id = keyboard.next();
-                    Movie m = licenses.findByLicenseID(id);
+                    Movie m = data.findByLicenseID(id);
                     if (m != null){
                         System.out.println(m.toExpirationString());
                     }
@@ -44,9 +48,10 @@ public class Main {
                     }
                     break;
                 case "5":
+
                     System.out.println("Enter movie title:");
                     String title = keyboard.next();
-                    Movie result = movieTree.search(title);
+                    Movie result = titleTree.search(title);
                     if (result != null) {
                         System.out.println(result.toExpirationString());
                     } else {
@@ -54,7 +59,7 @@ public class Main {
                     }
                     break;
                 case "6":
-                    HistoryManager.printHistory();
+                    Data.printHistory();
                     break;
                 default:
                     System.out.println("Please type in a valid number response.");
