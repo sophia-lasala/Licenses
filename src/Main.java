97ccb122f537ecc84cebb0547b3baf23bd857639
Sophia LaSala <sophia.lasala@outlook.com>
2025-12-16 19:02:07 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 package Streaming;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -11,9 +13,12 @@ public class Main {
         String userInput;
         Data data = new Data ();
         TitleTree titleTree = new TitleTree();
+        CostTree costTree = new CostTree();
 
         System.out.println("Importing CSV file...");
         data.buildAllStructures();
+        titleTree.buildFromList(data.getMovieList());
+        costTree.buildFromList(data.getMovieList());
         ArrayList<Movie> movieList = data.getMovieList();
         LinkedList<Movie> expirationList = data.getExpirationList();
         Queue<Movie> pendingQueue = data.getPendingQueue();
@@ -21,11 +26,11 @@ public class Main {
         System.out.println("Complete!");
 
         System.out.println("\nWould you like to: (1) Access all data (2) Access all licenses (3) Review licenses that need " +
-                "to be renewed soon (4) Access License IDs (5) Format Data (6) Access History (7) Exit");
+                "to be renewed soon (4) Access License IDs (5) Search for a Title (6) Access History (7) Exit");
 
         userInput = keyboard.next();
 
-        while (!(userInput.equalsIgnoreCase("7"))) {
+        while (!(userInput.equalsIgnoreCase("8"))) {
             switch (userInput) {
                 case "1":
                     data.printMovieList(movieList);
@@ -35,22 +40,61 @@ public class Main {
                     break;
                 case "3":
                     data.printPendingQueue();
+                    System.out.println("Would you like to renew any licenses on this list? (1) Yes (2) No");
+                    userInput = keyboard.next();
+                    while (userInput.equalsIgnoreCase("1")){
+                        LocalDate expirationDate, renewalDate;
+                        Movie firstMovie = pendingQueue.peek();
+                        System.out.println(firstMovie);
+                        System.out.println("Would you like to renew this license? (1) Yes (2) No");
+                        userInput = keyboard.next();
+                        if (userInput.equalsIgnoreCase("1")){
+                            LocalDate newRenewalDate = LocalDate.now();
+
+                            System.out.println("What is the new expiration date? (MM/dd/yyyy)");
+                            String expirationInput = keyboard.next();
+                            LocalDate newExpirationDate = LocalDate.parse(expirationInput, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+
+                            if (firstMovie != null) {
+                                String licenseID = firstMovie.getLicenseID();
+                                data.renewQueue(licenseID, newRenewalDate, newExpirationDate);
+                            }
+                        }
+                    }
+
+                    System.out.println("Here is the new list:");
+                    data.printPendingQueue();
                     break;
                 case "4":
-                    System.out.println("Enter License ID:");
-                    String id = keyboard.next();
-                    Movie m = data.findByLicenseID(id);
-                    if (m != null){
-                        System.out.println(m.toExpirationString());
-                    }
-                    else {
-                        System.out.println("License not found.");
+                    System.out.println("Search by: (1) License ID  (2) Mapped ID");
+                    userInput = keyboard.next();
+
+                    if (userInput.equals("1")) {
+                        System.out.println("Enter License ID:");
+                        String id = keyboard.next();
+                        Movie m = data.findByLicenseID(id);
+                        if (m != null) {
+                            System.out.println("Mapped ID: " + m.getMappedID());
+                        } else {
+                            System.out.println("Mapped ID not found.");
+                        }
+                    } else if (userInput.equals("2")) {
+                        System.out.println("Enter Mapped ID (number):");
+                        int mappedId = keyboard.nextInt();
+                        Movie m = data.getMovieByNumber(mappedId);
+                        if (m != null) {
+                            System.out.println("License ID: " + m.getLicenseID());
+                        } else {
+                            System.out.println("License ID not found.");
+                        }
+                    } else {
+                        System.out.println("Invalid choice.");
                     }
                     break;
                 case "5":
-
                     System.out.println("Enter movie title:");
-                    String title = keyboard.next();
+                    keyboard.nextLine();
+                    String title = keyboard.nextLine();
                     Movie result = titleTree.search(title);
                     if (result != null) {
                         System.out.println(result.toExpirationString());
@@ -59,6 +103,19 @@ public class Main {
                     }
                     break;
                 case "6":
+                    System.out.println("What would you like to order based on? (1) Title (2) Cost (3) Views");
+                    userInput = keyboard.next();
+                    if (userInput.equalsIgnoreCase("1")){
+                        titleTree.printInOrder();
+                    }
+                    else if (userInput.equalsIgnoreCase("2")){
+                        costTree.printInOrder();
+                    }
+                    else{
+                        System.out.println();
+                    }
+                    break;
+                case "7":
                     Data.printHistory();
                     break;
                 default:
